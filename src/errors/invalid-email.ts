f30c45e644072727e290bb1860a102cc23eba833
Jessica Mulein <jessica@digitaldefiance.org>
2025-11-14 21:16:02 -0800

    v2.2.2

--- src/errors/invalid-email.ts
+++ src/errors/invalid-email.ts
@@ -1,20 +1,22 @@
 import { InvalidEmailErrorType } from '@digitaldefiance/ecies-lib';
-import { PluginTypedError } from '@digitaldefiance/i18n-lib';
+import { HandleableErrorOptions, TypedError, TypedHandleableError } from '@digitaldefiance/i18n-lib';
 import { SuiteCoreStringKey } from '../enumerations';
-import { getSuiteCoreI18nEngine, SuiteCoreComponentId } from '../i18n-setup';
+import { SuiteCoreComponentId } from '../i18n-setup';
 
-export class InvalidEmailError extends PluginTypedError<typeof InvalidEmailErrorType, SuiteCoreStringKey> {
-  constructor(type: InvalidEmailErrorType, language?: string) {
-    const engine = getSuiteCoreI18nEngine();
+export class InvalidEmailError extends TypedHandleableError<typeof InvalidEmailErrorType, SuiteCoreStringKey> {
+  public readonly email?: string;
+  constructor(type: InvalidEmailErrorType, email?: string, options?: HandleableErrorOptions, language?: string) {
+    const source = options?.cause instanceof Error ? options.cause : new Error();
     const reasonMap: Record<InvalidEmailErrorType, SuiteCoreStringKey> = {
       [InvalidEmailErrorType.Invalid]: SuiteCoreStringKey.Error_InvalidEmail,
       [InvalidEmailErrorType.Missing]: SuiteCoreStringKey.Error_InvalidEmailMissing,
       [InvalidEmailErrorType.Whitespace]:
         SuiteCoreStringKey.Error_InvalidEmailWhitespace,
     };
-    super(SuiteCoreComponentId, type, reasonMap, language, {
-      statusCode: 422,
+    super(SuiteCoreComponentId, type, reasonMap, source, { ...options, statusCode: options?.statusCode ?? 422 }, language, {
+      email: email ?? ''
     });
+    this.email = email;
     this.name = 'InvalidEmailError';
   }
 }
